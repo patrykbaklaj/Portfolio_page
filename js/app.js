a070556c46cce53ebee24046538d30be34809f77
Patryk Bakłaj <patrykbaklaj@gmail.com>
2017-06-03 17:56:30 +0200

    Added section contact and style changed

--- js/app.js
+++ js/app.js
@@ -42,37 +42,64 @@ $(document).ready(function() {
 
   // section second animation
   ;(function($, win) {
-  $.fn.inViewport = function(cb) {
-     return this.each(function(i,el){
-       function visPx(){
-         var H = $(this).height(),
-             r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
-         return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));
-       } visPx();
-       $(win).on("resize scroll", visPx);
-     });
-  };
-}(jQuery, window));
+    $.fn.inViewport = function(cb) {
+      return this.each(function(i,el){
+        function visPx(){
+          var H = $(this).height(),
+          r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+          return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));
+        } visPx();
+        $(win).on("resize scroll", visPx);
+      });
+    };
+  }(jQuery, window));
 
 
 
-$(".section_second_header").inViewport(function(px){
+  $(".section_second_header").inViewport(function(px){
     if(px) $(this).addClass("header_active") ;
     else $(this).removeClass("header_active") ;
-});
+  });
 
-$(".section_second_text").inViewport(function(px){
+  $(".section_second_text").inViewport(function(px){
     if(px) $(this).addClass("text_active") ;
     else $(this).removeClass("text_active") ;
-});
+  });
 
-$(".download").inViewport(function(px){
+  $(".download").inViewport(function(px){
     if(px) $(this).addClass("download_active") ;
     else $(this).removeClass("download_active") ;
-});
+  });
 
-// slideshow
-$('.carousel').carousel();
+  // slideshow
+  $('.carousel').carousel();
+
+  $('.icons_container').inViewport(function(px) {
+    if(px) {
+      $(this).find("div.skill_icons").each(function(index, el) {
+        $(el).addClass('skill' + index);
+      });
+    }
+    else {
+      $(this).find("div.skill_icons").each(function(index, el) {
+        $(el).removeClass('skill' + index);
+      });
+    }
+  });
+
+  $('#contact form').inViewport(function(px) {
+    if(px) {
+      $(this).find("*").each(function(index, el) {
+        $(el).addClass('skill' + index);
+      });
+    }
+    else {
+      $(this).find("*").each(function(index, el) {
+        $(el).removeClass('skill' + index);
+      });
+    }
+  });
+
 
 
 });
